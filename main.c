23e31e99e25b9874d727bd0b28e7842a02343f5a
Jan Kuliga <jtkuliga@gmail.com>
2021-01-20 17:07:30 +0100

    basic reset handler written

--- main.c
+++ main.c
@@ -1,34 +1,43 @@
-/*********************************************************************
-*                    SEGGER Microcontroller GmbH                     *
-*                        The Embedded Experts                        *
-**********************************************************************
+/*
+ *Bootloader's main file
+ */
+ 
+#include <stdint.h>
+#include "bootloader_mem_map.h"
+#include "system_MKL46Z4.h"
 
--------------------------- END-OF-HEADER -----------------------------
+/*
+ *Reset handler for bootloader, invoked during MCU's startup.
+ *Sets clocks, initializes .bss section with zeros, copies initialized 
+ *variables to .data section, branches to main().
+ */
+void Reset_Handler(void);
 
-File    : main.c
-Purpose : Generic application start
-
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-
-/*********************************************************************
-*
-*       main()
-*
-*  Function description
-*   Application entry point.
-*/
-int main(void) {
-  int i;
-
-  for (i = 0; i < 100; i++) {
-    printf("Hello World %d!\n", i);
-  }
-  do {
-    i++;
-  } while (1);
+int main(void)
+{
+        
 }
 
-/*************************** End of file ****************************/
+
+void Reset_Handler(void)
+{
+        SystemInit();
+        
+        uint32_t *bss = &_sbss;
+        for (bss; bss < &_ebss; bss++)
+                *bss = 0;
+                
+        uint32_t *data = &_sdata;
+        uint32_t *init_vars = &_etext;
+        for (data; data < &_edata; data++)
+                *data = *init_vars++;
+        
+        main();
+        
+        while (1);
+}
+        
+        
+        
+                                        
+
