8d1c24046e4fbc8018da0769423e3f7446bd75e8
Jan Kuliga <jtkuliga@gmail.com>
2021-01-23 20:19:15 +0100

    experimental function uart9600_poll_init

--- main.c
+++ main.c
@@ -20,6 +20,11 @@ enum uart_baudrate {
  *Initialize UART0.
  */
 void uart0_poll_init(enum uart_baudrate br);
+
+/*
+ *Initialize UART0 for HC-05 communication (UART0_RX on PTA15).
+ */
+void uart9600_poll_init(void);
  
 /*
  *Deinitialize UARTO.
@@ -45,7 +50,8 @@ __attribute__((naked)) void goto_userspace(uint32_t sp, uint32_t pc)
 
 int main(void)
 {
-        uart0_poll_init(__115200__);
+        //uart0_poll_init(__115200__);
+        uart9600_poll_init();
 
         uint8_t *userspace = (uint8_t*) &__ram_userspace_start__;
         while (1) {        
@@ -81,7 +87,22 @@ void uart0_poll_init(enum uart_baudrate br)
         UART0->C1 |= UART0_C1_ILT_MASK;
         UART0->C4 &= ~UART0_C4_OSR_MASK;
         UART0->C4 |= UART0_C4_OSR(15);	
-        UART0->C5 |= UART0_C5_BOTHEDGE_MASK;	
+        UART0->C5 |= 1 << UART0_C5_BOTHEDGE_SHIFT;	
+        UART0->C2 |= 1 << UART0_C2_RE_SHIFT;
+}
+
+void uart9600_poll_init(void)
+{
+        SIM->SOPT2 |= 1 << SIM_SOPT2_UART0SRC_SHIFT;
+        SIM->SCGC4 |= 1 << SIM_SCGC4_UART0_SHIFT;
+        SIM->SCGC5 |= 1 << SIM_SCGC5_PORTA_SHIFT;
+        PORTA->PCR[15] = PORT_PCR_MUX(3);
+        UART0->BDH = 0b1;
+        UART0->BDL = 0b00010001;
+        UART0->C1 |= UART0_C1_ILT_MASK;
+        UART0->C4 &= ~UART0_C4_OSR_MASK;
+        UART0->C4 |= UART0_C4_OSR(15);	
+        UART0->C5 |= 1 << UART0_C5_BOTHEDGE_SHIFT;	
         UART0->C2 |= 1 << UART0_C2_RE_SHIFT;
 }
 
@@ -122,6 +143,5 @@ void uart0_deinit(void)
         
         while (1);
 }       
-        
                                         
 
