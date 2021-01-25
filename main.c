3dd4b6e35ef35372a1513a974589c8b40143032c
Jan Kuliga <jtkuliga@gmail.com>
2021-01-25 18:46:52 +0100

    added led_init(), led_deinit() foos

--- main.c
+++ main.c
@@ -11,7 +11,7 @@
  *Be aware that these values are for this particular configuration
  *and are not inter-application-compatible.
  */
-enum uart_baudrate {
+enum baudrate {
          __115200__ = 23,
          __28800__ = 91
 };
@@ -19,13 +19,23 @@ enum uart_baudrate {
 /*
  *Initialize UART0.
  */
-void uart0_poll_init(enum uart_baudrate br);
+void uart0_poll_init(enum baudrate br);
 
 /*
  *Deinitialize UARTO.
  *Needs to be done before going to userspace.
  */
 void uart0_deinit(void);
+
+/*
+ *Initialize green led;
+ */
+void led_init(void);
+
+/*
+ *Deinitialize green led.
+ */
+void led_deinit(void);
 
 /*
  *Branch to userspace.
@@ -46,22 +56,32 @@ __attribute__((naked)) void goto_userspace(uint32_t sp, uint32_t pc)
 int main(void)
 
 {
-        uart0_poll_init(__115200__);
-        //uart9600_poll_init();
+        char *buf = "Welcome to BOO-OTA!\n\rBootloader program enabling "
+                     "OTA software update feature.\n\rMade by Jan Kuliga"
+                     "\n\rFollow this project here: https://github.com/kuliga/boo-ota\n\rTMP2\n";
 
+        uart0_poll_init(__115200__);
+        led_init();
+        
+        for (buf; *buf != 0; buf++) {
+                while(!(UART0->S1 & UART0_S1_TDRE_MASK));
+                UART0->D = (uint8_t) *buf;
+        }
+                
+        
         uint8_t *userspace = (uint8_t*) &__ram_userspace_start__;
         while (1) {        
                 while (UART0->S1 & UART0_S1_RDRF_MASK) 
                         *userspace++ = UART0->D;                       
   
-                if (UART0->S1 & UART0_S1_IDLE_MASK) {
+                 if (UART0->S1 & UART0_S1_IDLE_MASK) {
                         UART0->S1 = 1 << UART0_S1_IDLE_SHIFT;
                         goto exit;
                 } 
         }
-              
-        
+            
 exit:  ;
+       led_init();
        uart0_deinit();
        
        uint32_t *reset_fetch = (uint32_t*) &__ram_userspace_start__;
@@ -73,27 +93,43 @@ exit:  ;
        while (1);
 }
 
-void uart0_poll_init(enum uart_baudrate br)
+void uart0_poll_init(enum baudrate br)
 {
         SIM->SOPT2 |= 1 << SIM_SOPT2_UART0SRC_SHIFT;
         SIM->SCGC4 |= 1 << SIM_SCGC4_UART0_SHIFT;;
         SIM->SCGC5 |= 1 << SIM_SCGC5_PORTA_SHIFT;
         PORTA->PCR[1] = PORT_PCR_MUX(2);
+        PORTA->PCR[2] = PORT_PCR_MUX(2);
         UART0->BDH = 0;
         UART0->BDL = br;
         UART0->C1 |= UART0_C1_ILT_MASK;
         UART0->C4 &= ~UART0_C4_OSR_MASK;
         UART0->C4 |= UART0_C4_OSR(15);	
         UART0->C5 |= 1 << UART0_C5_BOTHEDGE_SHIFT;	
-        UART0->C2 |= 1 << UART0_C2_RE_SHIFT;
+        UART0->C2 |= UART0_C2_RE_MASK | UART0_C2_TE_MASK;
 }
 
 void uart0_deinit(void)
 {
+        PORTA->PCR[1] = PORT_PCR_MUX(0);
+        PORTA->PCR[2] = PORT_PCR_MUX(0);
         SIM->SCGC5 &= ~SIM_SCGC5_PORTA_MASK;
         SIM->SCGC4 &= ~SIM_SCGC4_UART0_MASK;
         SIM->SOPT2 &= ~SIM_SOPT2_UART0SRC_MASK;
 }
+
+void led_init(void)
+{
+        SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
+        PORTD->PCR[5] = PORT_PCR_MUX(1);
+        PTD->PDDR |= 1 << 5;
+}
+
+void led_deinit(void)
+{
+        PORTD->PCR[5] = PORT_PCR_MUX(0);
+        SIM->SCGC5 &= ~SIM_SCGC5_PORTD_MASK;
+}       
         
 /*
  *Reset handler, invoked during MCU's startup.
