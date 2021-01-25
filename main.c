c726c8c0bcf59c8c0ce57895e7e1a37f0d3c2e46
Jan Kuliga <jtkuliga@gmail.com>
2021-01-25 16:42:58 +0100

    get rid of experimental foo

--- main.c
+++ main.c
@@ -22,11 +22,6 @@ enum uart_baudrate {
 void uart0_poll_init(enum uart_baudrate br);
 
 /*
- *Initialize UART0 for HC-05 communication (UART0_RX on PTA15).
- */
-void uart9600_poll_init(void);
- 
-/*
  *Deinitialize UARTO.
  *Needs to be done before going to userspace.
  */
@@ -49,20 +44,22 @@ __attribute__((naked)) void goto_userspace(uint32_t sp, uint32_t pc)
 
 
 int main(void)
+
 {
-        //uart0_poll_init(__115200__);
-        uart9600_poll_init();
+        uart0_poll_init(__115200__);
+        //uart9600_poll_init();
 
         uint8_t *userspace = (uint8_t*) &__ram_userspace_start__;
         while (1) {        
                 while (UART0->S1 & UART0_S1_RDRF_MASK) 
-                        *userspace++ = UART0->D;
-                
+                        *userspace++ = UART0->D;                       
+  
                 if (UART0->S1 & UART0_S1_IDLE_MASK) {
                         UART0->S1 = 1 << UART0_S1_IDLE_SHIFT;
                         goto exit;
-                }
+                } 
         }
+              
         
 exit:  ;
        uart0_deinit();
@@ -79,26 +76,11 @@ exit:  ;
 void uart0_poll_init(enum uart_baudrate br)
 {
         SIM->SOPT2 |= 1 << SIM_SOPT2_UART0SRC_SHIFT;
-        SIM->SCGC4 |= 1 << SIM_SCGC4_UART0_SHIFT;
+        SIM->SCGC4 |= 1 << SIM_SCGC4_UART0_SHIFT;;
         SIM->SCGC5 |= 1 << SIM_SCGC5_PORTA_SHIFT;
         PORTA->PCR[1] = PORT_PCR_MUX(2);
         UART0->BDH = 0;
         UART0->BDL = br;
-        UART0->C1 |= UART0_C1_ILT_MASK;
-        UART0->C4 &= ~UART0_C4_OSR_MASK;
-        UART0->C4 |= UART0_C4_OSR(15);	
-        UART0->C5 |= 1 << UART0_C5_BOTHEDGE_SHIFT;	
-        UART0->C2 |= 1 << UART0_C2_RE_SHIFT;
-}
-
-void uart9600_poll_init(void)
-{
-        SIM->SOPT2 |= 1 << SIM_SOPT2_UART0SRC_SHIFT;
-        SIM->SCGC4 |= 1 << SIM_SCGC4_UART0_SHIFT;
-        SIM->SCGC5 |= 1 << SIM_SCGC5_PORTA_SHIFT;
-        PORTA->PCR[15] = PORT_PCR_MUX(3);
-        UART0->BDH = 0b1;
-        UART0->BDL = 0b00010001;
         UART0->C1 |= UART0_C1_ILT_MASK;
         UART0->C4 &= ~UART0_C4_OSR_MASK;
         UART0->C4 |= UART0_C4_OSR(15);	
